575686c95ffccadcfa2281a0834f1837f152930f
daniilbanan <daniil22091990@icloud.com>
2019-06-17 16:06:50 +0300

    Правки

--- result.php
+++ result.php
@@ -24,19 +24,8 @@
 
     $overRate = $rate + $rate / 2;
 
-    if ($minutes == 15 || $overMinutes == 15) {
-        $totalMinutes = $rate / 4;
-        $overTotalMinutes = $overRate / 4;
-    } elseif ($minutes == 30 || $overMinutes == 30) {
-        $totalminutes = $rate / 2;
-        $overTotalMinutes = $overRate / 2;
-    } elseif ($minutes == 45 || overMinutes == 45) {
-        $totalminutes = $rate - $rate / 4;
-        $overTotalMinutes = $overRate - $overRate / 4;
-    }
-
-    $resultOne = $rate * $hours + $totalMinutes;
-    $resultTotal = $resultOne + $overRate * $overHours + $overTotalMinutes;
+    $resultOne = $rate * $hours + $rate / 60 * $minutes;
+    $resultTotal = $resultOne + $overRate * $overHours + $rate / 60 * $overMinutes;
 
     if (is_numeric($resultOne)) {
         echo 'Сумма без переработок: <span class="res">' . $resultOne . '</span><br>' . 'Сумма с переработками: <span class="res">' . $resultTotal . '</span>';
