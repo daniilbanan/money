16b029305bd0a66201a801320204e74771d30971
Lobanov Daniil <lobanov.daniil@4px.ru>
2019-06-25 15:59:25 +0300

    Мелкие правки

--- js/function.js
+++ js/function.js
@@ -1,24 +1,24 @@
 //Вычисление формулы
 
 window.onload = function() {
-    var showRes = document.getElementById('showRes');
-    var hidden = document.querySelector('.hidden');
+    let showRes = document.getElementById('showRes');
+    let hidden = document.querySelector('.hidden');
 
     showRes.addEventListener('click', function show(evt) {
         evt.preventDefault();
         hidden.classList.add('show');
 
-        var rate = parseInt(document.getElementById('rate').value);
-        var hours = parseInt(document.getElementById('hours').value);
-        var minutes = parseInt(document.getElementById('minutes').value);
-        var overHours = parseInt(document.getElementById('overHours').value);
-        var overMinutes = parseInt(document.getElementById('overMinutes').value);
-        var overRate = rate + rate / 2;
+        let rate = parseInt(document.getElementById('rate').value.replace(/[^\d]/g, ''));
+        let hours = parseInt(document.getElementById('hours').value);
+        let minutes = parseInt(document.getElementById('minutes').value);
+        let overHours = parseInt(document.getElementById('overHours').value);
+        let overMinutes = parseInt(document.getElementById('overMinutes').value);
+        let overRate = rate + rate / 2;
 
-        var result = rate * hours + rate / 60 * minutes;
-        var result_str = result.toFixed(2);
-        var overResult = result + (overRate * overHours + overRate / 60 * overMinutes);
-        var overResult_str = overResult.toFixed(2);
+        let result = rate * hours + rate / 60 * minutes;
+        let result_str = result.toFixed(2);
+        let overResult = result + (overRate * overHours + overRate / 60 * overMinutes);
+        let overResult_str = overResult.toFixed(2);
 
         document.getElementById('result').innerHTML = result_str;
         document.getElementById('overResult').innerHTML = overResult_str;
